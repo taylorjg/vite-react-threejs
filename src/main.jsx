e133d28646fd40381a6dce2c0188f420c713acc5
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2024-09-19 11:14:14 +0100

    Add MUI

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+  responsiveFontSizes,
+} from "@mui/material";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 import { App } from "./App.jsx";
+
+const darkTheme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      mode: "dark",
+    },
+  })
+);
 
 const root = document.getElementById("root");
 
 createRoot(root).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
