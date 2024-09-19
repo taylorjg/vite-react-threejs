ca7ea0049113a3cec7c0e066a3e76219e3fe0d58
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2024-09-19 11:06:51 +0100

    Add prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import { App } from "./App.jsx";
+
+const root = document.getElementById("root");
+
+createRoot(root).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
